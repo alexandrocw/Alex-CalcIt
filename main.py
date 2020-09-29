9c184feab363ee1bfb433ce3796385ab14c62345
Alexandro Christian Wijaya <61381891+lKingDGl@users.noreply.github.com>
2020-09-30 00:02:30 +0700

    Update Alex CalcIt A.003
    Added subtract, multiply, and divide function.
    Support multiple expressions
    Support expression after get the result

--- main.py
+++ main.py
@@ -1,38 +1,102 @@
 import tkinter as tk
+import ast
+import operator as op
+
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul, ast.Div: op.truediv}
 
 root = tk.Tk()
-root.title("Alex CalcIt A.002")
+root.title("Alex CalcIt A.003")
 
 entry = tk.Entry(root, width=35, borderwidth=5)
 entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-array = []
+expression = ''
+
+
+def eval_expr(expr):
+    return eval_(ast.parse(expr, mode='eval').body)
+
+
+def eval_(node):
+    if isinstance(node, ast.Num):
+        return node.n
+    elif isinstance(node, ast.BinOp):
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))
+    elif isinstance(node, ast.UnaryOp):
+        return operators[type(node.op)](eval_(node.operand))
+    else:
+        raise TypeError(node)
 
 
 def button_click(number):
     entry.insert('end', number)
 
-def button_clear():
+
+def clear():
+    global expression
     entry.delete(0, 'end')
+    expression = ''
+
 
 def add():
+    global expression
+    if expression:
+        expression += '+'
+        return
     try:
-        current = int(entry.get())
-        array.append(current)
+        current = entry.get()
+        expression += current
+        expression += '+'
         entry.delete(0, 'end')
     except ValueError:
-        entry.insert(0, "Insert Number First!")
+        pass
+
+
+def subtract():
+    global expression
+    try:
+        current = entry.get()
+        expression += current
+        expression += '-'
         entry.delete(0, 'end')
+    except ValueError:
+        pass
+
+
+def multiply():
+    global expression
+    try:
+        current = entry.get()
+        expression += current
+        expression += '*'
+        entry.delete(0, 'end')
+    except ValueError:
+        pass
+
+
+def divide():
+    global expression
+    try:
+        current = entry.get()
+        expression += current
+        expression += '/'
+        entry.delete(0, 'end')
+    except ValueError:
+        pass
+
 
 def result():
-    sum = 0
-    current = int(entry.get())
-    array.append(current)
-    for num in array:
-        sum+=num
-    entry.delete(0, 'end')
-    array.clear()
-    entry.insert(0, sum)
+    global expression
+    try:
+        last = entry.get()
+        expression += last
+        entry.delete(0, 'end')
+        res = eval_expr(expression)
+        entry.insert('end', res)
+        expression = ''
+        expression += res
+    except (ValueError, TypeError):
+        pass
 
 
 # Make button
@@ -52,8 +116,11 @@ button_9 = tk.Button(root, text="9", padx=40, pady=20, command=lambda: button_cl
 button_0 = tk.Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
 button_add = tk.Button(root, text="+", padx=39, pady=20, command=add)
 button_equal = tk.Button(root, text="=", padx=90, pady=20, command=result)
-button_clear = tk.Button(root, text="Clear", padx=79, pady=20, command=button_clear)
+button_clear = tk.Button(root, text="Clear", padx=79, pady=20, command=clear)
 
+button_subtract = tk.Button(root, text="-", padx=41, pady=20, command=subtract)
+button_multiply = tk.Button(root, text="*", padx=40, pady=20, command=multiply)
+button_divide = tk.Button(root, text="/", padx=41, pady=20, command=divide)
 
 # Show button on screen
 
@@ -73,5 +140,9 @@ button_0.grid(row=4, column=0)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
 button_clear.grid(row=4, column=1, columnspan=2)
+
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
 
 root.mainloop()
